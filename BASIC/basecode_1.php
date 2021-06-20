9221fe17859a5c5d665f4193303a4bf7140f45d5
Mohed <mccdama@gmail.com>
2021-06-20 12:52:01 +0200

    basic_1

--- BASIC/basecode_1.php
+++ BASIC/basecode_1.php
@@ -26,20 +26,40 @@
     echo "<br>";
     echo "Hello, $my_string!<br>"; // Displays: Hello World!
     echo 'Hello, $my_string!<br>'; // Displays: Hello, $my_str!
-
-
-
     ?>
 
 
     <?php
-
-
-    echo '<pre>Hello\tWorld!</pre>';   // Displays: Hello\tWorld!
     echo "<pre>Hello\tWorld!</pre>"; // Displays: Hello   World!
+    echo '<pre>Hello\tWorld!</pre>';   // Displays: Hello\tWorld!
     echo 'I\'ll be back';            // Displays: I'll be back
+    echo "<hr>";
+    ?>
+
+    <?php
+    #  calc. number of characters inside a string
+    $my_calc_str = 'Welcome to my PHP webpage! ';
+    echo strlen($my_calc_str); // Outputs: 27
+    echo "<hr>";
+    ?>
 
 
+    <?php
+    # counts the number of words in a string.
+    $my_calc_str1 = 'The quick brown fox jumps over the lazy dog.';
+    echo str_word_count($my_calc_str1); // Outputs: 9
+    echo "<hr>";
+    ?>
+
+    <?php
+    #replacing text within string
+    $my_replace = 'If the facts do not fit the theory, change the facts.';
+
+    // Display replaced string
+    echo "<u>Orginal</u>: " . $my_replace . "<br>";
+    echo "<i>after change: </i> " . str_replace("the ", " a ", $my_replace, $countofrep) . "<strong> $countofrep </strong>" . '<br>';
+    //reversing a str
+    echo strrev($my_calc_str);
 
     ?>
 
