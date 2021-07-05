18d3f6b8e9459c18846502c9e5ed6b5c3f274e5b
Mohed <mccdama@gmail.com>
2021-07-05 21:08:47 +0200

    session

---
+++ ADVANCED/6_Session/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Advanced PHP - Session </title>
+    <link rel="icon" href="./../favicon.ico">
+</head>
+<style>
+    .img-box {
+        display: inline-block;
+        text-align: center;
+        margin: 0 15px;
+    }
+</style>
+
+<body>
+
+
+</body>
+
+</html>
