ff05d1a59e8bdc222e67f83730af8155059fb649
Mohed <mccdama@gmail.com>
2021-07-06 15:43:54 +0200

    new dynamic twig temp

--- Symfony_Framwork/quick_tour/src/Controller/DefaultController.php
+++ Symfony_Framwork/quick_tour/src/Controller/DefaultController.php
@@ -2,35 +2,37 @@
 // src/Controller/DefaultController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
     * @Route("/hello/{name}")
     */
     public function index($name)
     {
-        return new Response("Hello $name!");
+        //return new Response("Hello $name!");
+        return $this->render('default/index.html.twig', ['name'=>$name]);
     }
 
     /**
      * @Route("/simple")
      */
-    public function simple()
+    /*public function simple()
     {
         return new Response('Easy and Simple');
-    }
+    }*/
     /**
 +      * @Route("/lucky/number")
 +      */
-     public function number(): Response
+     /*public function number(): Response
     {
         $number = random_int(0, 100);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
-    }
+    }*/
 }
