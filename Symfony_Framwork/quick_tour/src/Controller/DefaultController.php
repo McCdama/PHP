30f4851ef14d59ea6599db66ed1e4f970edc6719
Mohed <mccdama@gmail.com>
2021-07-06 15:31:41 +0200

    install twig

--- Symfony_Framwork/quick_tour/src/Controller/DefaultController.php
+++ Symfony_Framwork/quick_tour/src/Controller/DefaultController.php
@@ -22,4 +22,15 @@ class DefaultController
     {
         return new Response('Easy and Simple');
     }
+    /**
++      * @Route("/lucky/number")
++      */
+     public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
 }
