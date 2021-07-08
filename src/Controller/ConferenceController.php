ce7909eff1cbdfd1a33f9757e2536c0d061da1c1
Mohed <mccdama@gmail.com>
2021-07-08 23:50:02 +0200

    Using Twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,46 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(Request $request): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        /* return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]); */
-        /*  return new Response(<<<EOF
-<html>
-        <body>
-            <img src="/images/under-construction.gif" />
-        </body>
-</html>
-EOF); */
-
-        $greet = '';
-        if ($name = $request->query->get('hello')) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-        // try both:
-        /* http://127.0.0.1:8001/ */
-        /* http://127.0.0.1:8001/?hello=Mohed */
-
-
-        return new Response(<<<EOF
-<html>
-        <body>
-        $greet
-            <img src="/images/under-construction.gif" />
-        </body>
-</html>
-EOF);
+        return new Response($twig->render('conference/index.html.twig', ['conferences' => $conferenceRepository->findAll()]));
     }
 }
