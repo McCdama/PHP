30fdcc1391b3dfc6c0965c66f0ca49334595b45f
Mohed <mccdama@gmail.com>
2021-07-10 16:52:21 +0200

    sessions

--- ADVANCED/6_Session/index.php
+++ ADVANCED/6_Session/index.php
@@ -9,15 +9,49 @@
     <link rel="icon" href="./../favicon.ico">
 </head>
 <style>
-    .img-box {
-        display: inline-block;
+    body {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: x-large;
+        max-width: 500px;
+        justify-content: center;
         text-align: center;
-        margin: 0 15px;
+        margin: auto;
+        background-color: #f5f5f5;
+        color: #2F4F4F;
     }
 </style>
 
 <body>
+    <?php
+    #start up the session
+    session_start();
+    #echo session_status();
+    #storing session data
+    $_SESSION['firstname'] = "Mohed";
+    $_SESSION['lastname'] = "Alrahbi";
 
+    echo "Hi, " . $_SESSION['firstname'] . ' ' . $_SESSION['lastname'];
+    echo "<hr>";
+    echo "<hr>";
+
+    #Destroying a Session
+
+    if (isset($_SESSION['lastname'])) {
+        unset($_SESSION['lastname']);
+    }
+    echo $_SESSION['firstname'];
+    #echo $_SESSION['lastname']; // --> undefined
+    echo "<hr>";
+    echo "<hr>";
+
+    #Completely destroying
+    session_destroy();
+    echo "Hi ";
+    echo $_SESSION['firstname']; // not destroyed!!
+    #echo $_SESSION['lastname']; // --> undefined
+    echo "<hr>";
+
+    ?>
 
 </body>
 
