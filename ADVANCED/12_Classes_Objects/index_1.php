4d07508e38a48ce09b5ad4d6cee7795eaed0d7ea
Mohed <mccdama@gmail.com>
2021-07-11 00:17:32 +0200

    no message

---
+++ ADVANCED/12_Classes_Objects/index_1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title> Classes & Objects </title>
+    <style type="text/css">
+        body {
+            font-family: 'Courier New', Courier, monospace;
+            font-size: x-large;
+            max-width: 500px;
+            justify-content: center;
+            text-align: center;
+            margin: auto;
+            background-color: #f5f5f5;
+            color: #2F4F4F;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+
+    ?>
+
+
+</body>
+
+</html>
