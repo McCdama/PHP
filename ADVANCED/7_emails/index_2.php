988a72b0f6726dd43840817d40a1f1f39ac0218d
Mohed <mccdama@gmail.com>
2021-07-10 17:39:58 +0200

    sending emails

---
+++ ADVANCED/7_emails/index_2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Advanced PHP - Email </title>
+    <link rel="icon" href="./../favicon.ico">
+</head>
+<style>
+    body {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: x-large;
+        max-width: 500px;
+        justify-content: center;
+        text-align: center;
+        margin: auto;
+        background-color: #f5f5f5;
+        color: #2F4F4F;
+    }
+</style>
+
+<body>
+    <?php
+    $to = 'm7md.ra7abi@gmail.com';
+    $subject = 'Marriage Proposal';
+    $from = 'm7md.ra7abi@gmail.com';
+
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    // Create email headers
+    $headers .= 'From: ' . $from . "\r\n" .
+        'Reply-To: ' . $from . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    // Compose a simple HTML email message
+    $message = '<html><body>';
+    $message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
+    $message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
+    $message .= '</body></html>';
+
+    // Sending email
+    if (mail($to, $subject, $message, $headers)) {
+        echo 'Your mail has been sent successfully.';
+    } else {
+        echo 'Unable to send email. Please try again.';
+    }
+    ?>
+
+</body>
+
+</html>
