b0dc867a620682b3c0fa69cee9f4ab211beb9496
Mohed <mccdama@gmail.com>
2021-07-22 23:59:48 +0200

    LIMIT Clause

---
+++ ADVANCED/PHP_MySQL/08_LIMIT_Clause.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PHP-MySQL - LIMIT Clause </title>
+    <link rel="icon" href="./../../favicon.ico">
+</head>
+<style>
+    body {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: x-large;
+        max-width: 500px;
+        justify-content: center;
+        text-align: center;
+        margin: auto;
+        background-color: #f5f5f5;
+        color: #2F4F4F;
+    }
+</style>
+
+<body>
+    <?php
+    # Limiting Result Sets
+    # to constrain the number of rows returned by the SELECT statement. (e.g: Pagination)
+
+    # SELECT column_name(s) FROM table_name LIMIT row_offset, row_count;
+
+    /* Attempt MySQL server connection. */
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=demo_DB", "root", "0000");
+        // Set the PDO error mode to exception
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("ERROR: Could not connect. " . $e->getMessage());
+    }
+
+    // Attempt select query execution
+    try {
+        $sql = "SELECT * FROM persons LIMIT 3";
+        $result = $pdo->query($sql);
+        if ($result->rowCount() > 0) {
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>id</th>";
+            echo "<th>first_name</th>";
+            echo "<th>last_name</th>";
+            echo "<th>email</th>";
+            echo "</tr>";
+            while ($row = $result->fetch()) {
+                echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['first_name'] . "</td>";
+                echo "<td>" . $row['last_name'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            // Free result set
+            unset($result);
+        } else {
+            echo "No records matching your query were found.";
+        }
+    } catch (PDOException $e) {
+        die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+    }
+
+    // Close connection
+    unset($pdo);
+    ?>
+</body>
+
+</html>
